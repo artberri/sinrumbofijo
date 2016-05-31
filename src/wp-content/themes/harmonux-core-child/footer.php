5124d2dd134887cad4b808441c0a6604a17ebbdd
Alberto Varela <alberto@berriart.com>
2016-05-31 22:57:04 +0200

    CHaning url

--- src/wp-content/themes/harmonux-core-child/footer.php
+++ src/wp-content/themes/harmonux-core-child/footer.php
@@ -9,7 +9,7 @@ if (check_position_of_component('sidebar', 'left'))
         <div id="footer-bottom">
             <div class="row">
                 <div class="large-16 columns footer-navigation">
-                    <p><a rel="me" href="http://www.sinrumbofijo.com">Sin Rumbo Fijo</a> is fully powered with free software by <a href="http://www.luiszuno.com"> Berriart</a> | 2007-2016 | <a href="http://creativecommons.org/licenses/by-nc-sa/3.0/" rel="license">Licencia CC BY NC SA</a></p>
+                    <p><a rel="me" href="http://www.sinrumbofijo.com">Sin Rumbo Fijo</a> is fully powered with free software by <a href="http://www.berriart.com"> Berriart</a> | 2007-2016 | <a href="http://creativecommons.org/licenses/by-nc-sa/3.0/" rel="license">Licencia CC BY NC SA</a></p>
                 </div>
             </div>
         </div>
