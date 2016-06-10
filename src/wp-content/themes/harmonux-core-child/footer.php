00f14539503f90bfb0b43b50bd847fd020b672d5
Alberto Varela <alberto@berriart.com>
2016-06-10 23:45:43 +0200

    Adding footer style

--- src/wp-content/themes/harmonux-core-child/footer.php
+++ src/wp-content/themes/harmonux-core-child/footer.php
@@ -10,9 +10,14 @@ if (check_position_of_component('sidebar', 'left'))
             <div class="row">
                 <div class="large-16 columns footer-navigation">
                     <p><a rel="me" href="http://www.sinrumbofijo.com">Sin Rumbo Fijo</a> is powered by <a href="http://www.berriart.com"> Berriart</a> | 2007-2016 | <a href="http://creativecommons.org/licenses/by-nc-sa/3.0/" rel="license">Licencia CC BY NC SA</a>
-                    <br>
-                    Only free software was used and no kitties were harmed in the making of this website.
                     </p>
+                </div>
+            </div>
+            <div class="footer-claim">
+                <div class="row">
+                    <div class="large-16 columns ">
+                        <p>Only free software was used and no kitties were harmed in the making of this website.</p>
+                    </div>
                 </div>
             </div>
         </div>
